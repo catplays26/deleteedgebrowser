ae870b63e8cd8d3dd711cf3f139d84466d8ae97f
catplays26 <48035783+catplays26@users.noreply.github.com>
2020-06-21 09:18:15 -0400

    Update s1.cmd

--- deleteedge/s1.cmd
+++ deleteedge/s1.cmd
@@ -1,4 +1,5 @@
 cd C:\Windows\SystemApps
-cacls C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe /E /P everyone:f
+icacls icacls c:pssecret.docx /setowner %username% /T /C /L /Q
+icacls "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe" /grant %username%:(OI)(CI)F /T
 takeown /f C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe /r /d y
 RD /S /Q C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe

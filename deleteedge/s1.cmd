3345d9a1cc62460a08c0aa6e1d4ea27c3f92ce60
catplays26 <48035783+catplays26@users.noreply.github.com>
2020-07-08 13:11:37 -0400

    Update s1.cmd

--- deleteedge/s1.cmd
+++ deleteedge/s1.cmd
@@ -10,3 +10,10 @@ takeown /F "C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\Microsoft.Mi
 takeown /F "C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\MicrosoftEdge.exe"
 rd "C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 del /f "C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\MicrosoftEdge.exe"
+@echo Complete! Ignore all errors, Edge was deleted successfully
+pause
+color 07
+title C:\Windows\System32\cmd.exe
+@echo Press any key to exit cmd.
+pause
+exit
